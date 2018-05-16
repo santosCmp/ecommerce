fce62ae95a524b53cd7fb2823fb805bbec3e51ac
SantosCmp <elvecio0800@gmail.com>
2018-05-16 11:15:15 -0300

    criamos a classe Page

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <?php 
-
+//dependencias do composer 
+//o que o meu projeto precisa?
 require_once("vendor/autoload.php");
+//namesapces e nomes das classes
+use \Slim\Slim;
+use \Hcode\Page;
+// Slim usado para rotas
+$app = new Slim();
 
-$app = new \Slim\Slim();
-
+//linha 12 qual e a rota que estou chamando?  dentro do bloco 
 $app->config('debug', true);
-
+//quando chamar via get na pasta raiz sem nenhuma rota executa essa função 
 $app->get('/', function() {
     
-	echo "OK";
+	//adiconar o header na tela
+	$page = new Page(); 
+
+	// adicionar o h1 
+	$page->setTpl("index"); 
 
 });
-
+//tudo carregado? beleza então vamos rodar
 $app->run();
 
  ?>
